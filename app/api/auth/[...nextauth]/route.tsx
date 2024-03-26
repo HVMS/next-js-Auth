a9a46260003ba5b43d020a33e28b00d3ae0c8ac2
HVMS <viralsid2330@gmail.com>
2024-03-26 01:55:39 -0300

    Route.tsx file trying to fix : NextAuthOptions

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,33 +1,16 @@
-// import NextAuth from "next-auth/next";
-// import { type NextAuthOptions } from "next-auth";
-// import GithubProvider from "next-auth/providers/github";
-
-// export const authOptions: NextAuthOptions = {
-//     providers: [
-//         GithubProvider({
-//             clientId: process.env.github_id as string,
-//             clientSecret: process.env.github_secret as string,
-//         }),
-//     ],
-// };
-
-// const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST};
-
-import NextAuth from "next-auth";
-import { NextApiHandler } from "next";
+import NextAuth from "next-auth/next";
+import { type NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-const authHandler: NextApiHandler = (req, res) => {
-    return NextAuth(req, res, {
-        providers: [
-            GithubProvider({
-                clientId: process.env.github_id as string,
-                clientSecret: process.env.github_secret as string,
-            }),
-        ],
-    });
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.github_id as string,
+            clientSecret: process.env.github_secret as string,
+        }),
+    ],
 };
 
-export default authHandler;
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST};

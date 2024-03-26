717a3f9d376862a4c6f070edc30c685e7e63b019
HVMS <viralsid2330@gmail.com>
2024-03-26 01:53:52 -0300

    Route.tsx file trying to fix : NextAuthOptions

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -15,19 +15,19 @@
 
 // export { handler as GET, handler as POST};
 
-import NextAuth from "next-auth/next";
-import { type NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
+import { NextApiHandler } from "next";
 import GithubProvider from "next-auth/providers/github";
 
-export const authOptions: NextAuthOptions = {
-    providers: [
-        GithubProvider({
-            clientId: process.env.github_id as string,
-            clientSecret: process.env.github_secret as string,
-        }),
-    ],
+const authHandler: NextApiHandler = (req, res) => {
+    return NextAuth(req, res, {
+        providers: [
+            GithubProvider({
+                clientId: process.env.github_id as string,
+                clientSecret: process.env.github_secret as string,
+            }),
+        ],
+    });
 };
 
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST};
+export default authHandler;

f88beee8a438c87069a43247ea5c4bf14c3f0c27
HVMS <viralsid2330@gmail.com>
2024-03-26 01:50:11 -0300

    Add GitHub authentication and update login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,15 +6,14 @@ import { redirect } from "next/navigation";
 
 export default function Login() {
     const { data: session } = useSession();
-    if (session !== null) {
+    if (session) {
         redirect("/admin");
     }
 
     return (
         <div>
-            <p>This is login page - you can either choose an option for GitHub or Google Authentication.</p>
-            <button onClick={() => signIn("github")}>Sign in with github</button>
-            <button onClick={() => signIn("google")}>Sign in with google</button>
+            <p>This is login page - you can either choose an option for GitHub Authentication.</p>
+            <button onClick={() => signIn("github")}> Sign in with GitHub </button>
         </div>
     )
 
